9844f599209a864b6525ac2c9f2aa9b0e4137835
fredikasanjaya <116275592+fredikasanjaya@users.noreply.github.com>
2022-12-21 04:03:55 -0800

    Add files via upload

---
+++ Motor.java
@@ -0,0 +1,14 @@
+public class Motor implements RodaDua{
+    public String brand;
+    public int Tahun;
+
+    @Override
+    public void powerON(){
+        System.out.println("(ON) Motor Menyala");
+    }
+
+    @Override
+    public void powerOFF(){
+        System.out.println("(OFF) Motor Dimatikan");
+    }
+}
